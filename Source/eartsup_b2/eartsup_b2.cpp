b738572ec68991a205085ed073d234f323b02fa4
Geremy Cambus <cambusgeremy@outlook.fr>
2026-01-18 23:01:27 +0100

    Initial Commit

---
+++ Source/eartsup_b2/eartsup_b2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "eartsup_b2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, eartsup_b2, "eartsup_b2" );
